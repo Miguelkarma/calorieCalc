ac06d7d105dad3b45bc0e63a1ba6647cb01d0f7f
Miguelkarma <santos.p.miguel46@gmail.com>
2024-11-20 22:41:28 +0800

    update sql

--- sql/caloriecalc.sql
+++ sql/caloriecalc.sql
@@ -22,9 +22,9 @@ USE `caloriecalc`;
 -- Dumping structure for table caloriecalc.admins
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
@@ -39,14 +39,14 @@ INSERT INTO `admins` (`id`, `email`, `username`, `password`, `created_at`) VALUE
 CREATE TABLE IF NOT EXISTS `calculation_history` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `calculation` text COLLATE utf8mb4_general_ci NOT NULL,
+  `calculation` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `calculation_history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table caloriecalc.calculation_history: ~10 rows (approximately)
+-- Dumping data for table caloriecalc.calculation_history: ~9 rows (approximately)
 INSERT INTO `calculation_history` (`id`, `user_id`, `calculation`, `created_at`) VALUES
 	(1, 2, '[]', '2024-09-28 09:58:11'),
 	(2, 2, '[]', '2024-09-28 10:03:29'),
@@ -61,7 +61,12 @@ INSERT INTO `calculation_history` (`id`, `user_id`, `calculation`, `created_at`)
 	(11, 7, '{"Maintain Weight":"3275","Mild Weight Loss":"2948","Weight Loss":"2620","Extreme Weight Loss":"1998"}', '2024-10-20 09:56:17'),
 	(12, 7, '{"Maintain Weight":"367130","Mild Weight Loss":"330417","Weight Loss":"293704","Extreme Weight Loss":"223949"}', '2024-10-20 12:39:44'),
 	(13, 4, '{"Maintain Weight":"5149","Mild Weight Loss":"4634","Weight Loss":"4119","Extreme Weight Loss":"3141"}', '2024-10-21 12:01:48'),
-	(14, 5, '{"Maintain Weight":"41682","Mild Weight Loss":"37514","Weight Loss":"33346","Extreme Weight Loss":"25426"}', '2024-10-21 13:47:33');
+	(14, 5, '{"Maintain Weight":"41682","Mild Weight Loss":"37514","Weight Loss":"33346","Extreme Weight Loss":"25426"}', '2024-10-21 13:47:33'),
+	(15, 8, '{"Maintain Weight":"2598","Mild Weight Loss":"2338","Weight Loss":"2079","Extreme Weight Loss":"1585"}', '2024-11-07 08:17:32'),
+	(16, 11, '{"maintainWeight":"26429","mildWeightLoss":"23786","weightLoss":"21143","extremeWeightLoss":"16121"}', '2024-11-20 06:36:02'),
+	(17, 11, '{"maintainWeight":"26429","mildWeightLoss":"23786","weightLoss":"21143","extremeWeightLoss":"16121"}', '2024-11-20 06:36:02'),
+	(18, 11, '{"maintainWeight":"37991","mildWeightLoss":"34192","weightLoss":"30393","extremeWeightLoss":"23174"}', '2024-11-20 06:36:06'),
+	(19, 11, '{"maintainWeight":"18582","mildWeightLoss":"16724","weightLoss":"14866","extremeWeightLoss":"11335"}', '2024-11-20 06:36:28');
 
 -- Dumping structure for table caloriecalc.tbl_calorie
 CREATE TABLE IF NOT EXISTS `tbl_calorie` (
@@ -70,9 +75,9 @@ CREATE TABLE IF NOT EXISTS `tbl_calorie` (
   `calorie_date` date NOT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`tbl_calorie_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
--- Dumping data for table caloriecalc.tbl_calorie: ~6 rows (approximately)
+-- Dumping data for table caloriecalc.tbl_calorie: ~18 rows (approximately)
 INSERT INTO `tbl_calorie` (`tbl_calorie_id`, `calorie_amount`, `calorie_date`, `user_id`) VALUES
 	(1, 1234, '2024-06-24', 1),
 	(2, 12341, '2024-06-25', 2),
@@ -80,7 +85,21 @@ INSERT INTO `tbl_calorie` (`tbl_calorie_id`, `calorie_amount`, `calorie_date`, `
 	(7, 5125, '2024-10-15', 7),
 	(9, 123, '2024-10-21', 7),
 	(10, 4, '2024-10-30', 7),
-	(11, 213, '2024-10-21', 5);
+	(11, 213, '2024-10-21', 5),
+	(15, 100, '2024-11-07', 8),
+	(16, 120, '2024-11-07', 8),
+	(17, 50, '2024-11-08', 8),
+	(19, 2000, '2024-11-06', 8),
+	(20, 50, '2024-11-06', 8),
+	(24, 1223, '2024-11-14', 9),
+	(25, 21423, '2024-11-15', 9),
+	(26, 23123, '2024-11-13', 9),
+	(27, 2500, '2024-11-14', 10),
+	(28, 550, '2024-11-13', 10),
+	(29, 2250, '2024-11-19', 10),
+	(30, 222, '2024-11-20', 11),
+	(31, 2355, '2024-11-21', 11),
+	(32, 2523, '2024-11-19', 11);
 
 -- Dumping structure for table caloriecalc.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -89,20 +108,28 @@ CREATE TABLE IF NOT EXISTS `users` (
   `birth_date` date DEFAULT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `bio` text,
-  `profile_picture` varchar(255) DEFAULT NULL,
+  `bio` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `profile_picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` enum('active','inactive','blocked') DEFAULT 'active',
+  `reset_pin` int DEFAULT NULL,
+  `pin_expiration` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table caloriecalc.users: ~1 rows (approximately)
-INSERT INTO `users` (`id`, `email`, `birth_date`, `username`, `password`, `bio`, `profile_picture`, `created_at`, `updated_at`, `status`) VALUES
-	(2, '123@gmail.com', '2024-10-21', 'bitch', '$2y$10$fLNZUoSO0Pp825wM/IvfA.vq8NRz.rMkmnLeQV9pjOD9zp6VSKC/K', 'k', NULL, '2024-10-21 00:52:22', '2024-10-21 01:12:59', 'active'),
-	(3, 'tolonges@gmail.com', '2024-10-21', 'okinawa', '$2y$10$.UWvS/PgGIYvTx3nQh7l..3iVQYgB3rC8IjRI6NyrQi5u9y4t2eJm', 'hey', NULL, '2024-10-21 01:25:02', '2024-10-21 01:30:57', 'active'),
-	(4, 'paul@gmail.com', '2024-10-21', '123', '$2y$10$CBQfouqR.aaCsCHs/PqZdux2PGxAinfUJujtgg7vPc.x1dhBzoS6O', '123', NULL, '2024-10-21 11:38:17', '2024-10-21 12:43:15', 'active'),
-	(5, 'fas@gmail.com', '2024-10-22', 'migz', '$2y$10$B7rgHWuiF8GBRBycFUEIaOGET6/X8Hr2nJ74G8KXq2YQ.2MlPYC/S', 'e', '../imagesicon1.png', '2024-10-21 12:53:41', '2024-10-21 13:15:20', 'active');
+-- Dumping data for table caloriecalc.users: ~10 rows (approximately)
+INSERT INTO `users` (`id`, `email`, `birth_date`, `username`, `password`, `bio`, `profile_picture`, `created_at`, `updated_at`, `status`, `reset_pin`, `pin_expiration`) VALUES
+	(2, '123@gmail.com', '2024-10-21', 'bitch tahimik lang sa umpisa', '$2y$10$fLNZUoSO0Pp825wM/IvfA.vq8NRz.rMkmnLeQV9pjOD9zp6VSKC/K', 'k', NULL, '2024-10-21 00:52:22', '2024-10-22 08:49:46', 'active', NULL, NULL),
+	(3, 'tolonges@gmail.com', '2024-10-21', 'okinawa', '$2y$10$.UWvS/PgGIYvTx3nQh7l..3iVQYgB3rC8IjRI6NyrQi5u9y4t2eJm', 'hey', NULL, '2024-10-21 01:25:02', '2024-10-21 01:30:57', 'active', NULL, NULL),
+	(4, 'paul@gmail.com', '2024-10-21', '123', '$2y$10$CBQfouqR.aaCsCHs/PqZdux2PGxAinfUJujtgg7vPc.x1dhBzoS6O', '123', NULL, '2024-10-21 11:38:17', '2024-10-21 12:43:15', 'active', NULL, NULL),
+	(5, 'fas@gmail.com', '2024-10-22', 'joyjoy', '$2y$10$fgkx2RV3zORgdCIIMk0SduCxfOgCQCOtmR1t3Am0yLRa4PfXWt1CK', 'mahilig sa royal blood na taga educ', '../images160571631_188394742753259_9113123428697261947_n.jpg', '2024-10-21 12:53:41', '2024-10-22 08:14:54', 'active', NULL, NULL),
+	(6, 'kalbo@gmail.com', '2024-10-22', 'ramen', '$2y$10$PoyXcGBhX4OavRRODE.qeut7Qgv6kX8hlaGe4F9WMr3ykvAGoxaBK', 'erap', '../imagesWIN_20240930_20_35_13_Pro.jpg', '2024-10-22 08:41:26', '2024-10-22 08:47:21', 'active', NULL, NULL),
+	(7, '246@gmail.com', '2024-10-23', 'eljaycstll', '$2y$10$.U3vJfeb838pLJ9cWDWm6eNrLFS/SmkNHGm2TpqZjGGUeLLyk6.sO', NULL, NULL, '2024-10-23 11:08:29', '2024-10-23 11:08:29', 'active', NULL, NULL),
+	(8, 'kalo@gmail.com', '2024-11-14', 'luigi', '$2y$10$mYRRntfH/BEFYAWJoFrwHOV8GdrxmK7e/lzK7GwMIFc4nDJBTahlq', 'kalo', '../imagesbayanihan.jpg', '2024-11-07 07:42:29', '2024-11-07 08:47:44', 'active', NULL, NULL),
+	(9, 'samplesample@gmail.com', '2024-11-15', 'i', '$2y$10$0cZYaqrplZrTGDP6cD5ZaedKovI4eUPwVzmoISioLuxhSuNVLlase', 'u', '../images0123582_jose-rizal.png', '2024-11-14 07:06:40', '2024-11-19 08:51:39', 'active', NULL, NULL),
+	(10, 'bsit4@gmail.com', '1996-01-14', 'joserizal', '$2y$10$d584LvAe9ZcXTa0qETMIo.7dPrdbWOM1UOy/gE425TrT.BXKsvJYi', 'Tell', '../images16997134_0.webp', '2024-11-14 08:18:10', '2024-11-14 08:20:53', 'active', NULL, NULL),
+	(11, 'thegreatjohn999@gmail.com', '2024-11-19', 'oki man', '$2y$10$AYwy9H5OPZ68qlEWgNXf.uU1rHw68JeziDguJVKS7luylGucUFTkC', '', '../images4k-minimalist-hd-background-wallpaper-82740.jpg', '2024-11-19 08:25:20', '2024-11-20 14:32:44', 'active', 176148, '2024-11-20 22:37:27');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
